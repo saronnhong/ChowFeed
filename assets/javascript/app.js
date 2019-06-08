e1109248f9a72009918ce4d7f6335dcd47b608a1
Saron <saronnhong@gmail.com>
2019-06-08 09:48:02 -0700

    added a little localstorage

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,4 @@
+
 // Search Button Protocol
 $("#searchButton").on("click", function (event) {
     // search recipe (food2fork)
@@ -5,33 +6,38 @@ $("#searchButton").on("click", function (event) {
     var API_Key = "2e49e2c85c1e845bbc21c3ea12d350f7";
     var queryURL = "https://www.food2fork.com/api/search?key=" + API_Key + "&q=" + searchTerm + "&page=1";
 
-    console.log(queryURL);
+    //console.log(queryURL);
 
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
-        console.log(response);
-        $("#results").text(JSON.stringify(response));
+        // console.log(response);
+
+        // $("#results").text(JSON.stringify(response));
     });
 
     // search Yelp for restauratn
-    var queryURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term="+ searchTerm + "&location=sandiego&sort_by=rating";
+    var queryURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=" + searchTerm + "&location=sandiego&sort_by=rating";
 
-console.log(queryURL);
-$.ajax({
-    url: queryURL,
-    headers: {
-        'Authorization': 'Bearer k5B2o56mcAHxciwOt2ZfMkycA27iEJXTcgKa4Nl4dgJsMC1dfeD-az31R-2U_qhHAn91iU_hVcb75hVFDFq8y7kslgCTTW_CAuAc_c2XvRdsZj10800YMCu8VX75XHYx',
-    },
-    method: "GET"
-}).then(function (response) {
-    for (var i=0; i < 10; i++)
-    console.log(response);
-    console.log(response.businesses[i].name);
-    console.log(response.businesses[i].image_url);
-    console.log(response.businesses[i].url);   //url to Yelp
+    //console.log(queryURL);
+    $.ajax({
+        url: queryURL,
+        headers: {
+            'Authorization': 'Bearer k5B2o56mcAHxciwOt2ZfMkycA27iEJXTcgKa4Nl4dgJsMC1dfeD-az31R-2U_qhHAn91iU_hVcb75hVFDFq8y7kslgCTTW_CAuAc_c2XvRdsZj10800YMCu8VX75XHYx',
+        },
+        method: "GET"
+    }).then(function (response) {
+        for (var i = 0; i < 10; i++)
+        console.log(response);
+        console.log(response.businesses[i].name);
+        console.log(response.businesses[i].image_url);
+        console.log(response.businesses[i].url);   //url to Yelp
+
+        localStorage.setItem("name", response.businesses[i].name);
+        localStorage.setItem("business image URL", response.businesses[i].image_url);
+        localStorage.setItem("yelp url", response.businesses[i].url);
+    });
+
 });
-    
- });
 
